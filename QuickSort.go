35f3c833f08572d2702c25adc3ded26f89fdef8a
lanyilee <2269146219@qq.com>
2018-10-21 09:46:34 +0800

    quicksort parttwo

--- QuickSort.go
+++ QuickSort.go
@@ -1,8 +1,11 @@
 package main
 
-import "crypto/md5"
+import (
+	"fmt"
+	"strconv"
+)
 
-func QuickSort(A []*int, p int, r int) {
+func QuickSort(A []int, p int, r int) {
 	if p >= r {
 		return
 	}
@@ -12,10 +15,30 @@ func QuickSort(A []*int, p int, r int) {
 
 }
 
-func Partition(A []*int, p int, r int) int {
-
+func Partition(A []int, p int, r int) int {
+	key := A[r]
+	i := p
+	j := p
+	for j < r {
+		if A[j] < key { //遍历的A[j]数小于key就交换,保证A[i]的左边小于key，右边大于key
+			tem := A[i]
+			A[i] = A[j]
+			A[j] = tem
+			i++
+		}
+		j++
+	}
+	A[r] = A[i]
+	A[i] = key
+	return i
 }
 
 func main() {
-
+	A := []int{3, 1, 21, 5, 2, 8, 4, 6, 9, 13, 10, 15, 7}
+	QuickSort(A, 0, len(A)-1)
+	fmt.Println()
+	for _, a := range A {
+		s := strconv.Itoa(a) + ","
+		fmt.Print(s)
+	}
 }
