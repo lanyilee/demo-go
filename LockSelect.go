644b265941e92a4a201f165b0b813b4f62c604e7
lanyi <lanyilee@qq.com>
2018-10-24 13:34:30 +0800

    异步，锁

--- LockSelect.go
+++ LockSelect.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	runtime.GOMAXPROCS(1)
+	//int_chan := make(chan int)
 	int_chan := make(chan int, 1)
 	string_chan := make(chan string, 1)
-	int_chan <- 1 //如果int_chan为无缓冲，则主线程会堵塞,即此行之下代码都无法执行
+	int_chan <- 1 //如果int_chan为无缓冲，则主线程会堵塞,即此行之下代码都无法执行,除非有其他线程 <-int_chan,取走1
 	string_chan <- "hello"
+	fmt.Println(len(int_chan))
 	select {
 	case value := <-int_chan:
 		fmt.Println(value)
--- Syncrwmuter.go
+++ Syncrwmuter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -11,27 +12,36 @@ type threadSafeSet struct {
 }
 
 func (set *threadSafeSet) Iter() <-chan interface{} {
-	ch := make(chan interface{})
+	//ch := make(chan interface{})
+	ch := make(chan interface{}, 3)
+	//fmt.Println(len(ch))
+	//ch<-1
+	//fmt.Println(len(ch))
+	//ch<-1
+	//fmt.Println(len(ch))
+	//ch<-1
+	//fmt.Println(len(ch))
 	go func() {
 		set.RLock()
-		for elem := range set.s {
-			ch <- elem
+		for elem, value := range set.s {
+			ch <- value
+			fmt.Println("elem:" + strconv.Itoa(elem))
+			//开始这里困惑为什么len(ch)在两次循环后才是1，原因是第一次流入之后，被外面主线程“v := <-th.Iter() ”,流出去了
+			fmt.Println(len(ch))
 		}
 		close(ch)
 		set.RUnlock()
-		fmt.Println(len(ch))
 	}()
-	<-ch
-	fmt.Println(len(ch) + 10)
 	return ch
 }
 
 func main() {
 
 	th := threadSafeSet{
-		s: []interface{}{"1", "2"},
+		s: []interface{}{"1", "2", "3"},
 	}
+	//这里会堵塞，等待th.Iter()流进数据，之后才流出到v
 	v := <-th.Iter()
+	fmt.Println("over")
 	fmt.Println(v)
-	//fmt.Sprintf("%s%v","ch",v)
 }
